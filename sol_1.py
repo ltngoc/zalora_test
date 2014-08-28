da7bcbb44a1ae4402228c4f996139cd47e34fcac
ltngoc <ltngoc@comp.nus.edu.sg>
2014-08-28 09:52:03 -0400

    Update sol_1.py

--- sol_1.py
+++ sol_1.py
@@ -1,6 +1,21 @@
-from array import *
-
-array = array(array('a','b','c'), array('d','e','f'))
+array = [['a','b','c'], ['d','e','f']]
 for sub in array:
-		for e in sub:
-			print(e)
+    for element in sub:
+        print(element)
+        
+import string
+fin = open('~/data_engineer_zalora/programming_tasks/top10_sample.csv','r')
+lines = fin.readlines()
+brandList = []
+for ll in lines:
+    temp = ll.strip('",[,]\n')
+    brands = temp.split(",")
+    for br in brands:
+        brandList.append(br)
+
+brand_count = dict((brand, brandList.count(brand)) for brand in brandList)
+import operator
+brandCount_list = brand_count.items()
+sorted_brandCount = sorted(brandCount_list, key = operator.itemgetter(1), reverse=True)
+for entry in sorted_brandCount:
+    print(entry)
